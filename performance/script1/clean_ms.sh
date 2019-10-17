35e2e0bc8c7065a6407e644ce632618a024d306f
timcornwell <realtimcornwell@gmail.com>
2019-10-17 14:30:16 +0100

    Error in number of levels

--- performance/script1/clean_ms.sh
+++ performance/script1/clean_ms.sh
@@ -3,7 +3,7 @@
 
 # wsclean -weight uniform -log-time -size 20480 20480 -scale 1.7578125asec -niter  20000 -mgain 0.8 -auto-threshold 3 -pol xx ../GLEAM_A-team_EoR0_no_errors.ms
 
-cp ../clean_ms.py .
+cp ../../clean_ms.py .
 python ./clean_ms.py --ngroup 1 --nworkers 8 --weighting uniform --context wprojectwstack --nwslabs 15 \
 --mode pipeline --niter 1000 --nmajor 5 --fractional_threshold 0.2 --threshold 0.01 \
 --amplitude_loss 0.25 --deconvolve_facets 8 --deconvolve_overlap 16 --restore_facets 4 \
