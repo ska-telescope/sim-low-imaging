e3a672081d6512329e0e28b590d6410984ea35a3
timcornwell <realtimcornwell@gmail.com>
2019-11-08 15:57:02 +0000

    More command line args

--- testbed_ms.py
+++ testbed_ms.py
@@ -1,6 +1,7 @@
 """
 
-python testbed_ms.py --msname data/EoR0_20deg_24.MS --weighting uniform --context 2d --fov 0.2
+python testbed_ms.py --msname data/EoR0_20deg_24.MS --weighting natural --context 2d --fov 0.05
+
 """
 
 import argparse
@@ -44,6 +45,7 @@ if __name__ == "__main__":
     ########################################################################################################################
     
     parser = argparse.ArgumentParser(description='SKA LOW imaging using ARL')
+    parser.add_argument('--channels', type=int, nargs=2, default=[131,146], help='Channels to process')
     parser.add_argument('--context', type=str, default='2d', help='Imaging context')
     parser.add_argument('--msname', type=str, default='../data/EoR0_20deg_24.MS',
                         help='MS to process')
@@ -68,6 +70,8 @@ if __name__ == "__main__":
     target_ms = args.msname
     print("Target MS is %s" % target_ms)
     
+    ochannels = args.channels
+    print(ochannels)
     weighting = args.weighting
     nwplanes = args.nwplanes
     nwslabs = args.nwslabs
@@ -84,7 +88,7 @@ if __name__ == "__main__":
     # Read an MS and convert to Visibility format. This might need to be changed for another MS
     print("\nSetup of visibility ingest")
     msname = args.msname
-    bvis = create_blockvisibility_from_ms(msname, start_chan=131, end_chan=134)[-1]
+    bvis = create_blockvisibility_from_ms(msname, start_chan=ochannels[0], end_chan=ochannels[1])[-1]
     bvis.configuration.location = EarthLocation(lon="116.76444824", lat="-26.824722084", height=300.0)
     bvis.configuration.frame = ""
     bvis.configuration.receptor_frame = ReceptorFrame("linear")
@@ -93,14 +97,13 @@ if __name__ == "__main__":
     vis = convert_blockvisibility_to_visibility(bvis)
     
     frequency = numpy.array([numpy.average(vis.frequency)])
-    channel_bandwidth = numpy.array([numpy.sum(vis.frequency)])
+    channel_bandwidth = numpy.array([numpy.sum(vis.channel_bandwidth)])
     ####################################################################################################################
     
     print("\nSetup of images")
     phasecentre = SkyCoord(ra=0.0 * u.deg, dec=-27.0 * u.deg)
     
     advice = advise_wide_field(vis, guard_band_image=fov, delA=dela, verbose=True)
-    print(advice)
     
     if npixel is None:
         npixel = advice['npixels_min']
