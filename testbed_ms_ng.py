ed5443e91a9a0e23f79aeea0d4051ef55ffdc09e
timcornwell <realtimcornwell@gmail.com>
2019-11-13 13:37:55 +0000

    Update testbed_ms_ng

---
+++ testbed_ms_ng.py
@@ -0,0 +1,197 @@
+"""
+
+python testbed_ms.py --msname data/EoR0_20deg_24.MS --weighting natural --context 2d --fov 0.05
+
+"""
+
+import argparse
+import logging
+import pprint
+import sys
+import time
+
+import numpy
+
+import matplotlib as mpl
+
+#mpl.use('Agg')
+
+import matplotlib.pyplot as plt
+import astropy.units as u
+from astropy.coordinates import SkyCoord, EarthLocation
+
+from data_models.polarisation import ReceptorFrame, PolarisationFrame
+from processing_components.image.operations import qa_image, export_image_to_fits, show_image
+from processing_components.imaging.base import advise_wide_field, create_image_from_visibility
+from processing_components.visibility.base import create_blockvisibility_from_ms
+from processing_components.visibility.operations import convert_visibility_to_stokesI
+from processing_components.visibility.coalesce import convert_blockvisibility_to_visibility
+from processing_components.imaging.base import invert_ng
+from processing_components.imaging.weighting import weight_visibility
+
+pp = pprint.PrettyPrinter()
+
+log = logging.getLogger()
+log.setLevel(logging.INFO)
+log.addHandler(logging.StreamHandler(sys.stdout))
+mpl_logger = logging.getLogger("matplotlib")
+mpl_logger.setLevel(logging.WARNING)
+
+if __name__ == "__main__":
+    
+    start_epoch = time.asctime()
+    print("\nSKA LOW imaging using ARL\nStarted at %s\n" % start_epoch)
+    
+    ########################################################################################################################
+    
+    parser = argparse.ArgumentParser(description='SKA LOW imaging using ARL')
+    parser.add_argument('--channels', type=int, nargs=2, default=[131,146], help='Channels to process')
+    parser.add_argument('--context', type=str, default='2d', help='Imaging context')
+    parser.add_argument('--msname', type=str, default='../data/EoR0_20deg_24.MS',
+                        help='MS to process')
+    parser.add_argument('--local_directory', type=str, default='dask-workspace',
+                        help='Local directory for Dask files')
+    
+    parser.add_argument('--npixel', type=int, default=None, help='Number of pixels')
+    parser.add_argument('--threads', type=int, default=4, help='Number of threads')
+    parser.add_argument('--fov', type=float, default=1.0, help='Field of view in primary beams')
+    parser.add_argument('--cellsize', type=float, default=None, help='Cellsize in radians')
+    
+    parser.add_argument('--wstep', type=float, default=None, help='FStep in w')
+    parser.add_argument('--nwplanes', type=int, default=None, help='Number of wplanes')
+    parser.add_argument('--nwslabs', type=int, default=None, help='Number of w slabs')
+    parser.add_argument('--amplitude_loss', type=float, default=0.02, help='Amplitude loss due to w sampling')
+    parser.add_argument('--facets', type=int, default=1, help='Number of facets in imaging')
+    parser.add_argument('--oversampling', type=int, default=16, help='Oversampling in w projection kernel')
+    parser.add_argument('--weighting', type=str, default='natural', help='Type of weighting')
+    
+    args = parser.parse_args()
+    pp.pprint(vars(args))
+    
+    target_ms = args.msname
+    print("Target MS is %s" % target_ms)
+    
+    ochannels = args.channels
+    print(ochannels)
+    weighting = args.weighting
+    nwplanes = args.nwplanes
+    nwslabs = args.nwslabs
+    npixel = args.npixel
+    cellsize = args.cellsize
+    fov = args.fov
+    facets = args.facets
+    wstep = args.wstep
+    context = args.context
+    dela = args.amplitude_loss
+    threads=args.threads
+    
+    ####################################################################################################################
+    
+    # Read an MS and convert to Visibility format. This might need to be changed for another MS
+    print("\nSetup of visibility ingest")
+    msname = args.msname
+    bvis = create_blockvisibility_from_ms(msname, start_chan=ochannels[0], end_chan=ochannels[1])[-1]
+    bvis.configuration.location = EarthLocation(lon="116.76444824", lat="-26.824722084", height=300.0)
+    bvis.configuration.frame = ""
+    bvis.configuration.receptor_frame = ReceptorFrame("linear")
+    bvis.configuration.data['diameter'][...] = 15.0
+    print(bvis)
+    vis = convert_blockvisibility_to_visibility(bvis)
+    
+    frequency = numpy.array([numpy.average(vis.frequency)])
+    channel_bandwidth = numpy.array([numpy.sum(vis.channel_bandwidth)])
+    ####################################################################################################################
+    
+    print("\nSetup of images")
+    phasecentre = SkyCoord(ra=0.0 * u.deg, dec=-27.0 * u.deg)
+    
+    advice = advise_wide_field(vis, guard_band_image=fov, delA=dela, verbose=True)
+    
+    if npixel is None:
+        npixel = advice['npixels_min']
+    
+    if wstep is None:
+        wstep = 1.1 * advice['wstep']
+    
+    if nwplanes is None:
+        nwplanes = advice['wprojection_planes']
+    
+    if cellsize is None:
+        cellsize = advice['cellsize']
+    
+    print('Image shape is %d by %d pixels' % (npixel, npixel))
+    
+    ####################################################################################################################
+    
+    print("\nSetup of wide field imaging")
+    vis_slices = 1
+    actual_context = '2d'
+    support = 1
+    if context == 'wprojection':
+        # w projection
+        vis_slices = 1
+        support = advice[0]['nwpixels']
+        actual_context = '2d'
+        print("Will do w projection, %d planes, support %d, step %.1f" %
+              (nwplanes, support, wstep))
+    
+    elif context == 'wstack':
+        # w stacking
+        print("Will do w stack, %d planes, step %.1f" % (nwplanes, wstep))
+        actual_context = 'wstack'
+    
+    elif context == 'wprojectwstack':
+        # Hybrid w projection/wstack
+        nwplanes = int(1.5 * nwplanes) // nwslabs
+        support = int(1.5 * advice[0]['nwpixels'] / nwslabs)
+        support = max(15, int(3.0 * advice[0]['nwpixels'] / nwslabs))
+        support -= support % 2
+        vis_slices = nwslabs
+        actual_context = 'wstack'
+        print("Will do hybrid w stack/w projection, %d w slabs, %d w planes, support %d, w step %.1f" %
+              (nwslabs, nwplanes, support, wstep))
+    else:
+        print("Will do 2d processing")
+        # Simple 2D
+        actual_context = '2d'
+        vis_slices = 1
+        wstep = 1e15
+        nwplanes = 1
+    
+    model = create_image_from_visibility(vis, npixel=npixel, cellsize=cellsize, nchan=1,
+                                         frequency=frequency, channel_bandwidth=channel_bandwidth,
+                                         polarisation_frame=PolarisationFrame("stokesI"))
+    
+    print(model)
+    
+    # Perform weighting. This is a collective computation, requiring all visibilities :(
+    if weighting == 'uniform':
+        print("\nSetup of weighting")
+        print("Will apply uniform weighting")
+        vis_list = weight_visibility(vis, model)
+
+    # vis = convert_visibility_to_stokesI(vis)
+    
+    polmodel = create_image_from_visibility(vis, npixel=npixel, cellsize=cellsize, nchan=1,
+                                            frequency=frequency, channel_bandwidth=channel_bandwidth,
+                                         polarisation_frame=PolarisationFrame("stokesIQUV"))
+
+    start_epoch = time.time()
+    dirty, sumwt = invert_ng(vis.blockvis, polmodel, nthreads=threads)
+    print(qa_image(dirty))
+    print("Invert_ng finished in %.2f (seconds)" % (time.time() - start_epoch))
+    
+    show_image(dirty)
+    plot_name = target_ms.split('/')[-1].replace('.MS', '_dirty_ng.jpg')
+    plt.savefig(plot_name)
+    plt.show(block=False)
+    
+    dirty_name = target_ms.split('/')[-1].replace('.MS', '_dirty_ng.fits')
+    export_image_to_fits(dirty, dirty_name)
+
+#    polmodel.data[0,0,npixel//2,npixel//2] = 1.0
+#    vis = predict_ng(vis, polmodel)
+
+
+    
+    
